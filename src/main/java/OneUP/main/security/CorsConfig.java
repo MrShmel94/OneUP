9b30a83f18c266eca00ea4fbe582946200bcf5a0
vision <mrshmel94@gmail.com>
2025-05-01 15:30:41 +0200

    Big upd

--- src/main/java/OneUP/main/security/CorsConfig.java
+++ src/main/java/OneUP/main/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:3000", "https://oneup-front.vercel.app")
+                            .allowedOrigins("http://localhost:3000", "https://oneup-front.onrender.com")
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
                             .allowCredentials(true);
